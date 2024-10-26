a3df11c893b93860ae0392d35302d8db91cd2e0f
Tymur Shevchenko <timur.shevchenko7@gmail.com>
2024-10-26 15:59:11 +0300

    added loop while

--- MyConsoleProject.cpp
+++ MyConsoleProject.cpp
@@ -3,11 +3,15 @@
 using namespace std;
 
 int main() {
-    cout << "Input point coordinates: ";
-    double x, y; cin >> x >> y;
-    if ((y <= (x + 5)) &&
-        (y <= (-6 * x + 19)) &&
-        (9 * y >= (-5 * x - 25)))
-        cout << "This point is inside shaded area\n";
-    else cout << "This point is outside shaded area\n";
+
+    while (true)
+    {
+        cout << "Input point coordinates: ";
+        double x, y; cin >> x >> y;
+        if ((y <= (x + 5)) &&
+            (y <= (-6 * x + 19)) &&
+            (9 * y >= (-5 * x - 25)))
+            cout << "This point is inside shaded area\n";
+        else cout << "This point is outside shaded area\n";
+    }
 }

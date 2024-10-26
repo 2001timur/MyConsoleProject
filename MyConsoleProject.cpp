a278569e5228b7e04101ffe70e99f3d898b2f248
Tymur Shevchenko <timur.shevchenko7@gmail.com>
2024-10-26 15:57:21 +0300

    Add project files.

---
+++ MyConsoleProject.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Input point coordinates: ";
+    double x, y; cin >> x >> y;
+    if ((y <= (x + 5)) &&
+        (y <= (-6 * x + 19)) &&
+        (9 * y >= (-5 * x - 25)))
+        cout << "This point is inside shaded area\n";
+    else cout << "This point is outside shaded area\n";
+}
